45eaabba2ad4fccfbeb054d2a1ceb8ad067f7de2
LucaVallazza <Luvallazza@gmail.com>
2025-03-14 00:37:06 -0300

    feat: created keyword component

---
+++ src/components/ui/Keyword.tsx
@@ -0,0 +1,5 @@
+export const Keyword = ({ children }: { children: React.ReactNode }) => (
+  <span className="font-semibold text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-purple-500">
+    {children}
+  </span>
+);
